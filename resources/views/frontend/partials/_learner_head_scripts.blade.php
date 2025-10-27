088f7211913f6ccb9a99383262d4b59e8840ddbc
ely butabara <elybutabara@gmail.com>
2025-10-27 13:30:07 +0800

    disable gumlet

--- resources/views/frontend/partials/_learner_head_scripts.blade.php
+++ resources/views/frontend/partials/_learner_head_scripts.blade.php
@@ -3,12 +3,12 @@
     window.Laravel = '{{ json_encode(['csrfToken' => csrf_token()]) }}';
 </script>
 
-<script type="text/javascript">
+{{-- <script type="text/javascript">
     window.GUMLET_CONFIG = {
         hosts: [{
             current: "https://www.easywrite.se/",
             gumlet: "forfatterskolen.gumlet.com"
         }]
     };
-</script>
+</script> --}}
 <script async src="https://cdn.gumlet.com/gumlet.js/2.0/gumlet.min.js"></script>
