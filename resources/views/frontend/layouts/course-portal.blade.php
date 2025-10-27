1483dcab6eaff94ffc64877654beaa6d2a3f3ade
ely butabara <elybutabara@gmail.com>
2025-10-27 13:27:19 +0800

    do not use gumlet

--- resources/views/frontend/layouts/course-portal.blade.php
+++ resources/views/frontend/layouts/course-portal.blade.php
@@ -53,16 +53,6 @@
     @yield('styles')
 
     @include('frontend.partials._learner_head_scripts')
-
-    <script type="text/javascript">
-        window.GUMLET_CONFIG = {
-            hosts: [{
-                current: "https://www.easywrite.se/",
-                gumlet: "forfatterskolen.gumlet.com"
-            }]
-        };
-    </script>
-    <script async src="https://cdn.gumlet.com/gumlet.js/2.0/gumlet.min.js"></script>
 </head>
 <body>
 
