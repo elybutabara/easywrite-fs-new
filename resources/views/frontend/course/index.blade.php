52f490930f6fa52df4bea9d3f3715af3eefd4b50
ely butabara <elybutabara@gmail.com>
2025-10-27 13:33:29 +0800

    add gumlet

--- resources/views/frontend/course/index.blade.php
+++ resources/views/frontend/course/index.blade.php
@@ -82,3 +82,15 @@
 		</div> <!-- end courses-list-container -->
 	</div>
 @stop
+
+@section('scripts')
+<script type="text/javascript">
+	window.GUMLET_CONFIG = {
+		hosts: [{
+			current: "https://www.easywrite.se/",
+			gumlet: "forfatterskolen.gumlet.com"
+		}]
+	};
+</script>
+<script async src="https://cdn.gumlet.com/gumlet.js/2.0/gumlet.min.js"></script>
+@stop
